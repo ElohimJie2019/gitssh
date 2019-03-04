3b51a6d206fbc193d0e43ef0c7816479decda32b
zhangjie02 <zhangjie02@inke.cn>
2019-03-04 15:10:44 +0800

    初始化项目

---
+++ UseGitSsh/UseGitSsh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UseGitSsh
+//
+//  Created by zhangjie on 2019/3/4.
+//  Copyright © 2019年 Elohim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UseGitSsh/UseGitSsh/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  UseGitSsh
+//
+//  Created by zhangjie on 2019/3/4.
+//  Copyright © 2019年 Elohim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //初始化项目
+}
+
+
+@end
